fd273b8d6d56aaded05ea3f6bfd0b4f7015918c2
Agata <agata.react@gmail.com>
2022-10-29 14:58:06 +0200

    Filter function added - Final draft of the project

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ const generateTemplate = todo => {
     toDosList.insertAdjacentHTML('beforeend', `<li class= "list-group-item d-flex justify-content-between align-items-center">
 <span>${todo}</span><i class="far fa-trash-alt delete"></i>
 </li> `);
-
-
 }
-
 
 addForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -25,14 +22,34 @@ addForm.addEventListener('submit', e => {
     else {
         alert("please add toDo");
     }
-
 })
 
-
 // event delegation for deleting the items from the toDosList 
-
 toDosList.addEventListener('click', e => {
     if (e.target.classList.contains('delete')) {
         e.target.parentElement.remove();
     }
 })
+
+
+// searching for my toDos
+const filterToDos = searchToDo => {
+
+    const ArrayOfToDos = [...toDosList.children]
+
+    ArrayOfToDos
+        .filter(item => !item.textContent.toLowerCase().includes(searchToDo))
+        .forEach(item => item.classList.add('filtered'));
+
+    ArrayOfToDos
+        .filter(item => item.textContent.toLowerCase().includes(searchToDo))
+        .forEach(item => item.classList.remove('filtered'));
+
+};
+
+
+search.addEventListener('keyup', e => {
+    const searchToDo = e.target.value.trim().toLowerCase();
+    filterToDos(searchToDo);
+})
+
